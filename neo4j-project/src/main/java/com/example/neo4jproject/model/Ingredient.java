f484a7f959b2681e10f45ccc99036399d72d638b
DESKTOP-N81EU72\razva <razvantranca5@gmail.com>
2024-05-13 18:23:15 +0300

    3+4 req

--- neo4j-project/src/main/java/com/example/neo4jproject/model/Ingredient.java
+++ neo4j-project/src/main/java/com/example/neo4jproject/model/Ingredient.java
@@ -6,6 +6,8 @@ import org.springframework.data.neo4j.core.schema.GeneratedValue;
 import org.springframework.data.neo4j.core.schema.Id;
 import org.springframework.data.neo4j.core.schema.Node;
 
+import java.util.List;
+
 @Node
 @RequiredArgsConstructor
 @Data
--- neo4j-project/src/main/java/com/example/neo4jproject/service/IngredientService.java
+++ neo4j-project/src/main/java/com/example/neo4jproject/service/IngredientService.java
@@ -1,7 +1,15 @@
 package com.example.neo4jproject.service;
 
+import com.example.neo4jproject.model.Recipe;
+import com.example.neo4jproject.repository.IngredientRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class IngredientService {
+
 }
